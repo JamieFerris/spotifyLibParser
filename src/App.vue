ab956a06d2b3ca3b651507c1168e297c21322c1c
Pavel Nikolaidi <nps777@live.ru>
2022-10-19 17:12:34 +0300

    Initial commit feat: authorization with spotify account; implemented login

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div  class="header">
+    <h1>spotifyLibParser</h1>
+    <div v-if="loggedIn">
+      <div>Hello, username</div>
+      <button>logout</button>
+    </div>
+    <div v-else>
+      <button @click="handleLogin">login</button>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: localStorage.getItem('login_status') === 'logged_in',
+      access_token: localStorage.getItem('access_token')
+    }
+  },
+  methods: {
+    handleLogin() {
+      const CLIENT_ID = '48dc52ec43a54e1fbe1b6073e88e4214';
+      const REDIRECT_URI = 'http://127.0.0.1:5173/';
+      const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize';
+      const SCOPES = ['user-library-read','playlist-read-private','playlist-read-collaborative'];
+      const SCOPES_PARAM = SCOPES.join('%20');
+
+      //generating recommended state parameter:
+      let state = new Uint8Array(8);
+      crypto.getRandomValues(state);
+      state = Array.from(state, (dec) => {
+        return dec.toString(16).padStart(2,'0');
+      }).join('');
+      localStorage.setItem('state_param', state.toString());
+      localStorage.setItem('login_status', 'redirected')
+      window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&state=${state}&scope=${SCOPES_PARAM}&show_dialog=true`;
+    },
+    getParamsFromAuth(href) {
+      const paramsString = href.split('?')[1];
+      return paramsString.split('&').reduce((result, currentValue) => {
+        const [key, value] = currentValue.split('=');
+        result[key] = value;
+        return result;
+      }, {});
+    },
+
+  },
+  //handling login status
+  created() {
+    if (localStorage.getItem('login_status') === 'redirected') {
+      const params = this.getParamsFromAuth(window.location.href);
+      const generated_state = localStorage.getItem('state_param')
+      if (params.state === generated_state) {
+        localStorage.setItem('access_token', params.code);
+        this.loggedIn = true;
+        this.access_token = params.code;
+        localStorage.setItem('login_status', 'logged_in')
+
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
